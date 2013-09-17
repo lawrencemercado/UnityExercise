7d323e33a45f3d881f59f1f14839045c89dcf61e
lawrencemercado <christopher.mercado@klab.com>
2013-09-17 16:38:12 +0800

    Add Camera Movement from left to right. Add timer to some methods

---
+++ Exercise/Assets/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	public Transform player;
+	float time = 10f;
+	// Use this for initialization
+	void Start () {
+	//player = GetComponent<GameObject>();
+		iTween.LookFrom(this.gameObject, iTween.Hash("looktarget",player.transform.localPosition,"delay",5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//iTween.LookTo(this.gameObject,player.transform.localPosition,time);
+		
+	}
+}
